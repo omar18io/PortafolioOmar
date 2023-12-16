6a3ab8578cd14638f85b2cc1bf3c9e46a001ed42
Omarrivv <omar.rivera@vallegrande.edu.pe>
2023-12-16 18:24:09 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,44 @@
+const proyectos = [
+  {
+    titulo: "Pagina Web Centro de Varones",
+    descripcion:"Esta Pagina Web Fue Creada para Centro De Varones Para Tener Mas Informados A Los Padres De Familia Como Tambien Podemos Tener de Alumnos",
+    link: "https://www.youtube.com/watch?v=-IK4olKRqZY&t=18s",
+    fecha: "01/12/2023",
+    imagen: "python.png",
+    tecnologias: ["Javascript","HTML", "CSS", "BD", "Express", "NodeJS"],
+  },
+  {
+    titulo: "Juego De Mini Micraft",
+    descripcion:"Fue A Base De Un tutorial Que Me Pude Ver en Youtube",
+    link: "https://github.com/Omarrivv/juegoMiniMicraft.git",
+    fecha: "01/06/2023",
+    imagen: "fondo-python.png",
+    tecnologias: ["Python"],
+  },
+];
+
+const informacionPersonal = {
+  nombre: "Omar Felix Rivera Rosas",
+  subtitulo: "Desarrollador web",
+  imagen: "/img/imagen2-portafolio-removebg-preview.png",
+  otros: [
+    ["Nacionalidad", "Peruano"],
+    [
+      "Edad",
+      new Date(new Date() - new Date("2005/12/02")).getFullYear()-1970 + " años",
+    ],
+  ],
+  idiomas: [
+    ["Español", "Nativo"],["Inglés", "Intermedio"],["Ruso", "Principiante"],
+  ],
+  tecnologias: [
+    ["Python", 10],["JavaScript", 9],["Css", 9],["Typescript", 6],["Angular", 5
+    ],["Linux",7],["Node",7],["HTML",9],["AWS",6]
+  ],
+  redes: [
+    ["github", "https://github.com/Omarrivv"],
+    ["linkedin", "https://www.linkedin.com/in/omar-felix-rivera-rosas-149619274/"],
+    ["youtube", "https://www.youtube.com/@codefxomarroma"],
+    ["instagram", "https://www.youtube.com/@codefxomarroma"],
+  ]
+};
